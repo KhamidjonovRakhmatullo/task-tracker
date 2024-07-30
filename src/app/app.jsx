0e687e0593d01d8daafe41b7edd3c223dcdbc3ab
Rakhmatullo Khamidjonov <161606292+KhamidjonovRakhmatullo@users.noreply.github.com>
2024-07-30 21:51:07 +0900

    Update app.jsx

--- src/app/app.jsx
+++ src/app/app.jsx
@@ -34,7 +34,7 @@ const App = () => {
         {Array.isArray(toDoList) && toDoList.map((item)=>
         <NeedTodo>
            {/* {item.name} */}
-           aaabbb
+           This is my task
           <div style={{display: "flex", alignItems: "center"}}>
             <input type="checkbox" name="" id="" />
             <React.Fragment>
